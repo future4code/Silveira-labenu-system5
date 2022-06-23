cfcdf2325eb7454fc6748c4d77ef741a49546475
luissfmt <luis_melotavares@hotmail.com>
2022-06-23 14:53:27 -0300

    finalização das classes e métodos de turma

---
+++ src/endpoint/ClassController.ts
@@ -0,0 +1,54 @@
+import { Request, Response } from "express";
+import { ClassDataBase } from "../data/ClassDataBase";
+import { ClassModel } from "../model/ClassModel";
+
+export class ClassController {
+    public async createClass(req: Request, res: Response): Promise<void> {
+        try {
+            const { name, module } = req.body;
+            const classInfo = new ClassModel(name, module);
+
+            if(!name || !module) {
+                res.statusCode = 422;
+                throw new Error("Nome ou módulo de turma inválido");
+            }
+    
+            await new ClassDataBase().insertClass(classInfo);
+    
+            res.status(201).end();
+    
+        } catch (error: any) {
+            res.status(res.statusCode !== 200 ? res.statusCode : 500).send(error.sqlMessage || error.message);
+        }
+    };
+
+    public async getClasses(req: Request, res: Response): Promise<void> {
+        try {
+            const classes = await new ClassDataBase().selectClasses();
+
+            res.status(200).send(classes);
+
+        } catch (error: any) {
+            res.status(res.statusCode !== 200 ? res.statusCode : 500).send(error.sqlMessage || error.message);
+        }
+    };
+
+    public async updateClassModule(req: Request, res: Response): Promise<void> {
+        try {
+            const id = Number(req.params.id);
+            const module = Number(req.query.module);
+
+            if(!module) {
+                res.statusCode = 422;
+                throw new Error("Valor de módulo inválido.");
+            }
+
+            await new ClassDataBase().updateClassModule(module, id);
+
+            res.status(200).end();
+
+        } catch (error: any) {
+            res.status(res.statusCode !== 200 ? res.statusCode : 500).send(error.sqlMessage || error.message);
+        }
+    };
+};
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
 import { app } from "./app";
+import { ClassController } from "./endpoint/ClassController";
 
-import { createClass } from "./endpoint/createClass";
+const classController = new ClassController();
 
-app.post("/class", createClass);
+app.post("/class", classController.createClass);
+
+app.get("/class", classController.getClasses);
+
+app.patch("/class/:id", classController.updateClassModule);
